37c2d7d279e4434d425cde43648c93bd3c374fb8
laura13usero <laura13usero@gmail.com>
2025-05-15 10:46:09 +0200

    filtro

--- src/main/java/action/FiltrarHamburguesasAction.java
+++ src/main/java/action/FiltrarHamburguesasAction.java
@@ -1,16 +1,18 @@
 package action;
 
+import com.google.gson.Gson;
 import dao.HamburguesaDAO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.Producto;
 
+import java.io.IOException;
 import java.util.List;
 
 public class FiltrarHamburguesasAction implements Action {
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) {
-        String filtro = request.getParameter("filtro"); // puede ser "", "premium", "spicy" o "pollo"
+        String filtro = request.getParameter("filtro");
         HamburguesaDAO hamburguesaDAO = new HamburguesaDAO();
         List<Producto> hamburguesas;
 
@@ -20,12 +22,20 @@ public class FiltrarHamburguesasAction implements Action {
             hamburguesas = hamburguesaDAO.obtenerHamburguesasPorFiltro(filtro);
         }
 
-        request.setAttribute("hamburguesas", hamburguesas);
+        // Convertir la lista de hamburguesas a JSON usando Gson
+        Gson gson = new Gson();
+        String jsonHamburguesas = gson.toJson(hamburguesas);
 
+        // Configurar la respuesta para indicar que es JSON
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        // Escribir el JSON en la respuesta
         try {
-            request.getRequestDispatcher("/jsp/hamburguesas.jsp").forward(request, response);
-        } catch (Exception e) {
+            response.getWriter().write(jsonHamburguesas);
+        } catch (IOException e) {
             e.printStackTrace();
+            // Manejar el error adecuadamente (log, enviar error al cliente, etc.)
         }
     }
 }
